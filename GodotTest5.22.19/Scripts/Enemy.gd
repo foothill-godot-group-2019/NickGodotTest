23154cb3518606d9cb7ce9c2fece8f24bf2b9cf3
Nicccccc <34077481+Nicccccc@users.noreply.github.com>
2019-05-23 09:31:08 -0700

    Add files via upload
    Bug Fixes
    - character can now attack on spawn
    - fixed issue where hitbox wasn't flipping properly when player turned around

---
+++ GodotTest5.22.19/Scripts/Enemy.gd
@@ -0,0 +1,47 @@
+extends KinematicBody2D
+#enemy runSpeed
+export (float) var runSpeed = 80
+var velocity = Vector2()
+export(float) var jumpHeight = 40
+export(float) var jumpTime = 0.3
+var spriteFacingRight = true
+#pulls random integer that decides how skeleton acts
+var skeletonMode = 1
+
+onready var player = get_node("../Player")
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+	
+func _physics_process(delta):
+	var move = 0.0
+	var gravity = 2*jumpHeight/(jumpTime*jumpTime)
+
+	velocity.y += gravity*delta
+#select an enemy mode
+	if(skeletonMode == 1):
+		#print("skeleton is aggresive ", skeletonMode)
+	#movement
+		if(position.x < player.position.x - 15):
+			if(velocity.x > 0):
+				$Sprite.flip_h = false
+			move += 1
+			$AnimationPlayer.play("enemySkeletonWalk")
+			
+		if(position.x > player.position.x - 20):
+			if(velocity.x < 0):
+				spriteFacingRight = false
+			else:
+				spriteFacingRight = true
+			if(!spriteFacingRight):
+				$Sprite.flip_h = true
+			move -= 1
+			$AnimationPlayer.play("enemySkeletonWalk")
+	else:
+		#print("skeleton is defensive ", skeletonMode)
+		pass
+		
+	velocity.x = move * runSpeed
+	# -y is up, +y is down
+	velocity = move_and_slide(velocity, Vector2(0, -1))
